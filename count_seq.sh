dcf830818ac38dd0447518e8ebe6b399a858e883
junhongc <junhongc@stanford.edu>
2015-07-24 10:58:52 -0700

    slightly modified documentation

--- count_seq.sh
+++ count_seq.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # USAGE: bash count_seq.sh FASTA...
-# Description: counting number of sequences within a file
+# Description: counting number of sequences within a file, and it works
 grep -c "^>" $@
